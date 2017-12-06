df6c196bbecd0b2585c62e1f9673981ed0818cbb
KangDroid <hyunwoo286@gmail.com>
2017-12-06 12:21:50 +0900

    Change Delta height

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -506,7 +506,7 @@
   #define DELTA_DIAGONAL_ROD 217.0 // mm
 
   // height from z=0 to home position
-  #define DELTA_HEIGHT 339.65 // get this value from auto calibrate
+  #define DELTA_HEIGHT 339.25 // get this value from auto calibrate
 
   #define DELTA_ENDSTOP_ADJ { 0.0, 0.0, 0.0 } // get these from auto calibrate
 
