e4a867f9486dda84a308f806d383d69c03003ea1
KangDroid <hyunwoo286@gmail.com>
2018-03-30 23:50:39 +0900

    Change configuration files to anet's one
    PS1. Reduce homing feedrate for XY axis
    PS2. Enable PID AUTOTUNE Menu and enable PID Debug settings
    PS3. Enable NOZZLE PARK FEATURE
    PS4. ETC

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -40,7 +40,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "bugfix-2.0.x"
+  #define SHORT_BUILD_VERSION "AnetA8-2.0.x"
 
   /**
    * Verbose version identifier which should contain a reference to the location
@@ -75,7 +75,7 @@
   /**
    * Defines a generic printer name to be output to the LCD after booting Marlin.
    */
-  #define MACHINE_NAME "3D Printer"
+  #define MACHINE_NAME "DHSY-AnetA8"
 
   /**
    * The SOURCE_CODE_URL is the location where users will find the Marlin Source
