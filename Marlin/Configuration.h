6d8744e07066a36c45d9c05c3326b01e59911d95
KangDroid <hyunwoo286@gmail.com>
2018-04-12 00:24:40 +0900

    Add enable option for disabling software endstop

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1858,6 +1858,12 @@
 #endif
 
 /**
+ * Enable/Disable Software endstop options
+ *
+ */
+#define SHOW_SOFT_ENDSTOP_MENU
+
+/**
  * R/C SERVO support
  * Sponsored by TrinityLabs, Reworked by codexmas
  */
