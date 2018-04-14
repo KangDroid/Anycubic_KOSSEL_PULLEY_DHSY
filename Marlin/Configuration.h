33d8eff3e90e4897d82f47b76e7c22c1460b2e0a
KangDroid <hyunwoo286@gmail.com>
2018-04-14 14:01:53 +0900

    Revert "Add enable option for disabling software endstop"
    This reverts commit b1391ddd431158491a214be5745e6d7f6070be29.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1973,12 +1973,6 @@
 #endif
 
 /**
- * Enable/Disable Software endstop options
- *
- */
-#define SHOW_SOFT_ENDSTOP_MENU
-
-/**
  * R/C SERVO support
  * Sponsored by TrinityLabs, Reworked by codexmas
  */
