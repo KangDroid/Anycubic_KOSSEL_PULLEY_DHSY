7197cffc45724ba5a4b865201dcb64cbdf3d791f
KangDroid <hyunwoo286@gmail.com>
2019-07-02 15:29:06 +0900

    WIP: Add Fake Temperature for Developing Purposes
    Explanation:
    When developing, or working with Marlin firmware, we sometimes need to make a fake temperature for eg: testing print without any hardware configured.
    
    So this commit will add support for set fake temperature through GCode.
    
    Work In Progress.
    
    PS1: Initial Test-Suit commit
    PS2: Add Description
    PS3: Remove Test-Suite GCode 105, Now M981
    PS4: Only halt on real print
    PS5: Optimize && Cleanup
    PS6: Change variable name somewhat NOT KangDroid
    PS7: Parse T instead of E
    PS8: Change configuration name to FAKE_HOTEND_TEMPERATURE_SUPPORTED
    PS9: Add Fake Bed Temperature Support
    PS10: Parse S instead of T
    
    Change-Id: I64e8d504790d1e729d29695013a6b0ea7946dc21

---
+++ Marlin/src/gcode/develop/M982.cpp
@@ -0,0 +1,14 @@
+#include "../gcode.h"
+#include "../../module/temperature.h"
+
+void GcodeSuite::M982() {
+  if (parser.seen('S')) {
+    if (parser.value_int()) {
+      thermalManager.bed_fake_temperature_enabled = 1;
+      thermalManager.bed_fake_temperature_value = parser.value_int();
+    } else if (!parser.value_int()) {
+      thermalManager.bed_fake_temperature_enabled = 0;
+      thermalManager.bed_fake_temperature_value = 0.0;
+    }
+  }
+}
