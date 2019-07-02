0ab983cb01083b9cf8739b1c1f8c2538d7c0dc72
KangDroid <hyunwoo286@gmail.com>
2019-07-02 15:29:06 +0900

    WIP: Add Fake Temperature for Developing Purposes
    Explanation:
    When developing, or working with Marlin firmware, we sometimes need to make a fake temperature for eg: testing print without any hardware configured.
    
    So this commit will add support for set fake temperature through GCode.
    
    Work In Progress.
    
    PS1: Initial Test-Suit commit
    PS2: Add Description
    PS3: Remove Test-Suite GCode 105, Now M981
    PS4: Only halt on real print
    PS5: Optimize && Cleanup
    
    Change-Id: I64e8d504790d1e729d29695013a6b0ea7946dc21

--- Marlin/src/module/temperature.h
+++ Marlin/src/module/temperature.h
@@ -263,6 +263,11 @@ class Temperature {
 
     static hotend_info_t temp_hotend[HOTENDS];
 
+    #if ENABLED(FAKE_TEMPERATURE_SUPPORTED)
+      static float kangdroid_test_value;
+      static int kangdroid_test_enabled;
+    #endif
+
     #if HAS_HEATED_BED
       static bed_info_t temp_bed;
     #endif
@@ -564,7 +569,15 @@ class Temperature {
 
     FORCE_INLINE static float degHotend(const uint8_t e) {
       E_UNUSED();
-      return temp_hotend[HOTEND_INDEX].current;
+      #if ENABLED(FAKE_TEMPERATURE_SUPPORTED)
+        if (kangdroid_test_enabled) {
+          return kangdroid_test_value;
+        } else {
+          return temp_hotend[HOTEND_INDEX].current;
+        }
+      #else
+        return temp_hotend[HOTEND_INDEX].current;
+      #endif
     }
 
     #if ENABLED(SHOW_TEMP_ADC_VALUES)
